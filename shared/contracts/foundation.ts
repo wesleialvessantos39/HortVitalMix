e4bb0198eb410001db1c0b95a4669ea97d8b0019
wesleialvessantos39 <wesleialvessantos40@outlook.com>
2026-09-19 19:20:42 -0400

    chore(schema): atualizar versão lógica para 10

--- shared/contracts/foundation.ts
+++ shared/contracts/foundation.ts
@@ -37,4 +37,4 @@ export const ApiReadyResponseSchema = z
     reason: z.string().optional(),
   })
   .strict();
-export const FOUNDATION_SCHEMA_VERSION = 9;
+export const FOUNDATION_SCHEMA_VERSION = 10;
