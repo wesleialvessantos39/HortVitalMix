637ee47d03a986af56193f10d3d687fba62d6dfe
wesleialvessantos39 <wesleialvessantos40@outlook.com>
2026-09-21 15:30:32 -0400

    feat(trilha02): integrate server/types.d.ts

--- server/types.d.ts
+++ server/types.d.ts
@@ -3,6 +3,9 @@ export {};
 declare global {
   namespace Express {
     interface Request {
+      requestId: string;
+      clientIpHash: string;
+      adminActor?: import("./services/ConfigurationService.ts").ActorContext;
       actor: {
         userId: string;
         email: string | null;
