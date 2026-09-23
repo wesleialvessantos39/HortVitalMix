56c728ef737f23e7d2d9bdb873f22aa7f8e986a4
wesleialvessantos39 <wesleialvessantos40@outlook.com>
2026-09-22 22:26:08 -0400

    fix(trilha05): permitir PATCH administrativo no preflight

--- server/app.ts
+++ server/app.ts
@@ -26,7 +26,7 @@ app.use((req, res, next) => {
     res.set({
       "Access-Control-Allow-Origin": origin,
       "Access-Control-Allow-Credentials": "true",
-      "Access-Control-Allow-Methods": "GET,HEAD,POST,OPTIONS",
+      "Access-Control-Allow-Methods": "GET,HEAD,POST,PATCH,OPTIONS",
       "Access-Control-Allow-Headers": "Content-Type, X-Requested-With",
       Vary: "Origin",
     });
